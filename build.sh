fcd68f772672d078f7168526079e3466c443f30f
Giuppi <g.funicello@gmail.com>
2019-08-12 16:44:34 +0200

    fix env vars

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ echo "Preparation task"
 echo "Build Docker Image for $SERVICE_NAME"
 docker build -t $SERVICE_NAME .
 echo "Push to ECR Repository"
-docker tag $AWS_ECS_REPO_NAME:latest $AWS_ECR_REPOSITORY/$AWS_ECS_REPO_NAME:latest
-docker push $AWS_ECR_REPOSITORY/$AWS_ECS_REPO_NAME:latest
+docker tag $SERVICE_NAME:latest $AWS_ECR_REPOSITORY/$SERVICE_NAME:latest
+docker push $AWS_ECR_REPOSITORY/$SERVICE_NAME:latest
