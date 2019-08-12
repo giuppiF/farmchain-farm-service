d1c64678b8e55a41ee6ef3c8cf00c834b02e9d8c
Giuppi <g.funicello@gmail.com>
2019-08-12 17:13:29 +0200

    fix env vars

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ $(aws ecr get-login --no-include-email --region eu-west-1)
 echo "Preparation task"
 echo "Build Docker Image for $SERVICE_NAME"
 docker build -t $SERVICE_NAME .
-echo "Push to ECR Repository"
+echo "Push to $SERVICE_NAME to ECR Repository"
 docker tag $SERVICE_NAME:latest $AWS_ECR_REPOSITORY/$SERVICE_NAME:latest
 docker push $AWS_ECR_REPOSITORY/$SERVICE_NAME:latest
