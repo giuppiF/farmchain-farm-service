edf9622c6723ea784cc35b3aae27a25304795360
Giuppi <g.funicello@gmail.com>
2019-08-29 16:08:22 +0200

    prova deploy ecs

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo "Deploy to ECS"
-aws ecs update-service --force-new-deployment --service $SERVICE_NAME --region eu-west-1
+echo "Deploy $SERVICE_NAME to ECS"
+aws ecs update-service --force-new-deployment --cluster farmchain-cluster --service $SERVICE_NAME --region eu-west-1
