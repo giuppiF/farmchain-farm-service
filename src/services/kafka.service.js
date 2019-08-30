1735fb5368a8b799dbb44ba4c148aea0ca0ec13f
Giuppi <g.funicello@gmail.com>
2019-08-30 17:08:04 +0200

    old consumer

--- src/services/kafka.service.js
+++ src/services/kafka.service.js
@@ -62,7 +62,7 @@ const deleteProduct = async (repo,product) => {
 const kafkaService = (options, producer,client) => {
   var repo = options.repo;
   try {
-    const Consumer = kafka.HighLevelConsumer;
+    const Consumer = kafka.Consumer;
     var kafkaOptions = { topic: 'service.product', partition: 0 }
     var kafkaConsumerOptions =  {
       autoCommit: true,
